6e886755c86ca4894a92b518415f74c7ff78cf2f
Tamara-Doubra Precious <tamaradoubraprecious@gmail.com>
2024-10-20 00:54:21 +0100

    Foreign key amendment

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,7 +25,7 @@ CREATE TABLE Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
@@ -33,6 +33,6 @@ CREATE TABLE Order_Details(
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
